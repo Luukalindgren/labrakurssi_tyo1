5f5cdb39aafc25749896d21e533e949076dacd1d
konstanyman <80118317+konstanyman@users.noreply.github.com>
2023-03-27 14:01:03 +0300

    POST route done

--- routes/customers.js
+++ routes/customers.js
@@ -12,22 +12,36 @@ router.get('/', async (req, res) => {
     }
 })
 
-// Get one customer
+// Get one customer (in progress)
 router.get('/:id', (req, res) => {
     res.send(req.params.id)
 })
 
 // Creating new customer
-router.post('/', (req, res) => {
-    
+router.post('/', async (req, res) => {
+    const customer = new Customer({
+        name: req.body.name,
+        dateOfBirth: req.body.dateOfBirth,
+        joiningDate: req.body.joiningDate,
+        phoneNumber: req.body.phoneNumber,
+        email: req.body.email,
+        address: req.body.address,
+        membership: req.body.membership
+    })
+    try {
+        const newCustomer = await customer.save()
+        res.status(201).json(newCustomer)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
-// Updating existing customer
+// Updating existing customer (in progress)
 router.patch('/:id', (req, res) => {
     
 })
 
-// Deleting one customer
+// Deleting one customer (in progress)
 router.delete('/:id', (req, res) => {
     
 })
